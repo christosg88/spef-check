56bd677ab414667653c408084058bba07366e0a6
Christos Gkantidis <christosg8@gmail.com>
2023-03-03 01:11:06 +0100

    Remove redundant examples of basic parsers

--- examples/04-integer.cpp
+++
@@ -1,7 +0,0 @@
-#include <tao/pegtl.hpp>
-
-namespace pegtl = tao::pegtl;
-
-struct integer : pegtl::seq<pegtl::opt<pegtl::one<'+', '-'>>, // ('+'/'-')?
-                            pegtl::plus<pegtl::digit>         // digit+
-                            > {};
